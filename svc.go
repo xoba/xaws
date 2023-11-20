ca6c455be29612de0b744fc1e630048fe8f42cb2
mike andrews <mra@xoba.com>
2023-11-20 14:36:28 -0500

    import sfn client

--- svc.go
+++ svc.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/sesv2"
+	"github.com/aws/aws-sdk-go-v2/service/sfn"
 )
 
 func NewSesV2() (*sesv2.Client, error) {
@@ -23,6 +24,14 @@ func NewS3() (*s3.Client, error) {
 		return nil, err
 	}
 	return s3.NewFromConfig(c), nil
+}
+
+func NewSFN() (*sfn.Client, error) {
+	c, err := newConfig()
+	if err != nil {
+		return nil, err
+	}
+	return sfn.NewFromConfig(c), nil
 }
 
 const MaxEmailBytesSESV2 = 40 * 1024 * 1024
